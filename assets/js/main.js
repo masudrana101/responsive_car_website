b671e86650cd82e7957adcd4da477c9c00e9146c
Masud <masud.cse.sub@gmail.com>
2025-04-26 20:41:04 +0600

    2ndcommit
    Signed-off-by: Masud <masud.cse.sub@gmail.com>

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+/*================== SHOW MENU ==================*/
+
+
+/*============== REMOVE MENU MOBILE =============*/
+
+
+/*================= SWIPER HOME =================*/
+
+
+/*========= CHANGE BACKGRAOUND HEADER ===========*/
+
+
+/*=============== SHOW SCROLL UP ================*/
+
+
+/*======== SCROLL SELECTIONS ACTIVE LINK ========*/
+
+
+/*=========== SCROLL REVEAL ANIMATION ===========*/
