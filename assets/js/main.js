5b6f72eece0f77e8fb445f56020e0eba21f3c92b
Masud <masud.cse.sub@gmail.com>
2025-05-09 05:06:34 +0600

    5th
    Signed-off-by: Masud <masud.cse.sub@gmail.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,10 +34,25 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*================= SWIPER HOME =================*/
-
+const swiperHome = new Swiper('.home__swiper', {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        renderBullet: (index, className) => {
+          return '<span class="' + className + '">' + String(index + 1).padStart(2, '0') + "</span>";
+        },
+      },
+  
+  })
 
 /*========= CHANGE BACKGRAOUND HEADER ===========*/
-
+const bgHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offest is greater than 50 of the 
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', bgHeader)
 
 /*=============== SHOW SCROLL UP ================*/
 
